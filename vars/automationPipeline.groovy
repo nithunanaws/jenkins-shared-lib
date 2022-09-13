c98ecce37fe048364cca726875d771cf660c0689
nithyanandhamv <nithyanandhamv@pdisoftware.com>
2022-09-13 13:56:57 +0530

    Initial Commit

---
+++ vars/automationPipeline.groovy
@@ -0,0 +1,78 @@
+#!/usr/bin/env groovy
+
+def getdeploymentEnvironments(deploymentType) {
+    def map = [FUNCTIONAL:'INT,QAF',RELEASE:'INT,QAR,STG,PT,PROD']        
+    map[deploymentType] ?: 'INT,QAF'
+}
+
+def deployApp(deploymentType) {
+    def envs = getdeploymentEnvironments(deploymentType)
+    def deploymentEnvs = envs.split(',')
+    for(String deployEnv: deploymentEnvs) {
+        doDeploy(deployEnv, deploymentType)        
+    }
+}
+
+def doDeploy(deployEnv, deploymentType) {
+    if(deployEnv == "INT") {
+        stage("${deploymentType}-Build") {
+            steps {
+                script {
+                    echo "${deploymentType}-Build Stage"
+                }
+            }
+        }
+    }    
+    stage("${deployEnv}-Deploy") {        
+        steps {
+            script {
+                echo "${deployEnv}-Deploy Stage"
+            }
+        }
+    }
+    if(deployEnv == "INT") {
+        stage('Acceptance') {            
+            steps {
+                script {
+                    echo "Acceptance Stage"
+                }
+            }
+        }
+    }
+    if(deployEnv == "QAR") {
+        stage('Regression') {            
+            steps {
+                script {
+                    echo "Regression Stage"
+                }
+            }
+        }
+    }    
+}
+
+def call(body) {
+	
+	// evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent any
+
+        environment {            
+            deploymentType = "${pipelineParams.deploymentType}"
+        }
+
+        stages {
+            stage("${env.deploymentType}-Deployment") {
+                steps {
+                    script {
+                        deployApp(${env.deploymentType})
+                    }
+                }
+            }                        
+        }        
+    }
+}
