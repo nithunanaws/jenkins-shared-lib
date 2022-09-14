1d2a48b5dd7118df8688af3b952f8b1faf8144de
nithyanandhamv <nithyanandhamv@pdisoftware.com>
2022-09-14 18:56:56 +0530

    Review comments

--- vars/automationPipeline.groovy
+++ vars/automationPipeline.groovy
@@ -23,7 +23,7 @@ def call(body) {
 		post {            
             success {
                 script {
-                    currentBuild.description = "${pipelineParams.deploymentType}"                  
+                    currentBuild.description = "${pipelineParams.deploymentType} ? : 'FUNCTIONAL'"                  
                 }
             }            
         }
