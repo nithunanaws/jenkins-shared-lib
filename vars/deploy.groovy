e9a1f0c708ae8204cdd3a4be22ed5477509c0032
nithyanandhamv <nithyanandhamv@pdisoftware.com>
2022-09-20 12:17:19 +0530

    Remove error log

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -43,8 +43,7 @@ def runStage(def stageName, def jobName, def isStageDisabled) {
 	} catch(Exception e) {					
 		env.IS_STAGE_FAILED = 'true'
 		env.STAGE_FAILED = stageName
-		currentBuild.result = 'FAILURE'
-		error("${stageName} failed")
+		currentBuild.result = 'FAILURE'		
 	}
 }
 
