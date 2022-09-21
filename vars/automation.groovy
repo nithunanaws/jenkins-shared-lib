9e5f1dfe8ef6dcd1741c1972abbea9f0f8324dfc
nithyanandhamv <nithyanandhamv@pdisoftware.com>
2022-09-21 18:00:18 +0530

    Remove stage names

--- vars/automation.groovy
+++ vars/automation.groovy
@@ -118,7 +118,7 @@ def getFailedDeploymentEnv(def deploymentType) {
 
 def doDeploy(def deployEnv, def deploymentType, def pipelineParams, def jobName) {
     if(deployEnv == "INT") {
-        stage("${deploymentType}-Build") {
+        stage("Build") {
             script {								
 				env.IS_ANY_STAGE_FAILED = 'false'
 				def parameters = [
@@ -139,7 +139,7 @@ def doDeploy(def deployEnv, def deploymentType, def pipelineParams, def jobName)
 		}
     }
     if(deployEnv == "INT") {
-        stage("${deployEnv}-Acceptance") {
+        stage("Acceptance") {
             script {				
 				def parameters = []
 				runStage(env.STAGE_NAME, "${jobName}-Acceptance", pipelineParams.acceptanceDisabled, parameters)
@@ -148,7 +148,7 @@ def doDeploy(def deployEnv, def deploymentType, def pipelineParams, def jobName)
         }
     }
     if(deployEnv == "QAR") {
-        stage("${deployEnv}-Regression") {
+        stage("Regression") {
             script {				
 				def parameters = []
 				runStage(env.STAGE_NAME, "${jobName}-Regression", pipelineParams.regressionDisabled, parameters)
