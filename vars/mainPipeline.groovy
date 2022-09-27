bf73abcb163df037b4507abb0d5896e75af79c39
nithyanandhamv <nithyanandhamv@pdisoftware.com>
2022-09-27 16:14:54 +0530

    Separete jobs for deployment

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -117,7 +117,7 @@ def call(body) {
                 }
                 steps {
                     script {                        
-                        deploy("${env.JOB_NAME}-Functional")
+                        deploy("${env.JOB_NAME}-Functional-Deployment")
                     }
                 }
             }
@@ -129,7 +129,7 @@ def call(body) {
                 }
                 steps {
                     script {                        
-                        deploy("${env.JOB_NAME}-Release")
+                        deploy("${env.JOB_NAME}-Release-Deployment")
                     }
                 }
             }                       
